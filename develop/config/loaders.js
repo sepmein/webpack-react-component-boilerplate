05aa8d1c2275ec973d048abe3bb500bad64d3f1b
Spencer Zhang <sepmein@gmail.com>
2016-01-13 10:23:00 +0800

    added loaders

--- develop/config/loaders.js
+++ develop/config/loaders.js
@@ -1,4 +1,11 @@
 /**
  * Created by Spencer on 16/1/12.
  */
-module.exports = [];
+module.exports = [
+    {
+        test: /\.css$/, loader: "style-loader!css-loader"
+    },
+    {
+        test: /\.jsx$/, loader: "jsx-loader"
+    }
+];

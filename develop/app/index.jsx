e0b6511a8374ddf0e7cb918f036fb3ad668e7e26
Spencer Zhang <sepmein@gmail.com>
2016-09-25 10:03:11 +0800

    improved eslint

--- develop/app/index.jsx
+++ develop/app/index.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import {render} from 'react-dom'
 import {Provider} from 'react-redux';
-import {createStore,applyMiddleware} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import App from './component/main.jsx';
 import reducers from './component/reducers';
 import sagaMiddleWare from './component/sagas';
